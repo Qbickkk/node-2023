26abf5dd6c00873134cf5a46dca9570ad3ae606c
shulika <illia.shulika@gmail.com>
2024-04-08 21:57:44 -0700

    lesson 1

---
+++ helpers/helper.js
@@ -0,0 +1,10 @@
+// function helper() {
+//     console.log('I am helping you');
+//     console.log(__dirname, 'dirname');
+//     console.log(__filename, 'filename');
+//     console.log(process.cwd(), 'process.cwd');
+// }
+//
+// module.exports = {
+//     helper
+// }
